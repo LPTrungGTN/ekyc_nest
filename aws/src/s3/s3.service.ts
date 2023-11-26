4c8c2ac0e3ddab7a42ddf18d63d2d3a66297ebf6
LePhuongTrung <135190672+LPTrungGTN@users.noreply.github.com>
2023-11-26 13:25:46 +0700

    Add Rekognition service to S3Service

--- aws/src/s3/s3.service.ts
+++ aws/src/s3/s3.service.ts
@@ -1,24 +1,28 @@
-import { S3 } from 'aws-sdk';
+import { S3, Rekognition } from 'aws-sdk';
 import { Logger, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class S3Service {
   private s3: S3;
+  private rekognition: Rekognition;
 
   constructor(private configService: ConfigService) {
     this.s3 = new S3({
+      region: this.configService.get<string>('AWS_REGION'),
       accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
       secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
     });
+    this.rekognition = new Rekognition();
   }
 
   public async upload(file: {
     buffer: Buffer;
     originalname: string;
-  }): Promise<S3.ManagedUpload.SendData> {
+  }): Promise<Rekognition.DetectFacesResponse> {
     const { originalname } = file;
-    return await this.uploadS3(file.buffer, originalname);
+    await this.uploadS3(file.buffer, originalname);
+    return this.detectFaces(originalname);
   }
 
   private async uploadS3(
@@ -32,6 +36,30 @@ export class S3Service {
     };
     return new Promise((resolve, reject) => {
       this.s3.upload(params, (err, data) => {
+        if (err) {
+          Logger.error(err);
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  private async detectFaces(
+    fileName: string,
+  ): Promise<Rekognition.DetectFacesResponse> {
+    const params: Rekognition.DetectFacesRequest = {
+      Image: {
+        S3Object: {
+          Bucket: this.configService.get<string>('S3_BUCKET_NAME'),
+          Name: fileName,
+        },
+      },
+      Attributes: ['ALL'],
+    };
+
+    return new Promise((resolve, reject) => {
+      this.rekognition.detectFaces(params, (err, data) => {
         if (err) {
           Logger.error(err);
           reject(err.message);
